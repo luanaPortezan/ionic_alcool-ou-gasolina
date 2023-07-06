a04cb65270b2fb3a5dd8eed96a063c4f8514e8bd
Luana Portezan <luanaportezan@gmail.com>
2023-07-06 10:50:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_alcool-ou-gasolina',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
